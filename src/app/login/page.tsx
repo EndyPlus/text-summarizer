9c15f961b77e3ddff84f86f0020a464653b99867
EndyPlus <aapliusnin@gmail.com>
2026-01-21 20:28:03 +0200

    add: load priority to logo icon.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -87,6 +87,7 @@ export default function LoginPage() {
           alt="logo icon"
           width={60}
           height={60}
+          priority
         />
         <div className="py-6">
           <h3 className="text-heading tracking-base text-black-base leading-9 font-medium">
