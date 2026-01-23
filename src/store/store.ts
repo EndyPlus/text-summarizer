2530a1975b835390a63564334a2009f3e54bfd40
EndyPlus <aapliusnin@gmail.com>
2026-01-23 16:19:43 +0200

    install: zustang package.

---
+++ src/store/store.ts
@@ -0,0 +1,6 @@
+// import { create } from "zustand";
+
+// export const useTest = create((set) => ({
+//   testVal: 0,
+//   testLog: () => set((state) => ({ testVal: state.testVal + 1 })),
+// }));
