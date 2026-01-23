afd7975e5e4dd8429663153a03dc43034620d6ad
EndyPlus <aapliusnin@gmail.com>
2026-01-23 22:39:18 +0200

    add: mock data.

---
+++ src/mock/historySummaries.ts
@@ -0,0 +1,30 @@
+export const historySummaries = JSON.stringify([
+  {
+    id: 0,
+    summaryDate: "December 10, 2024 • 3:20 PM",
+    wordsCount: 77,
+    charactersCount: 538,
+    summaryText: `Naruto Uzumaki became the greatest shinobi of his time through
+          unparalleled power, relentless determination, and his ability to unite
+          and inspire others. Mastering Kurama's chakra and the Sage of Six
+          Paths' power, he achieved near-godlike abilities, defeating formidable
+          foes like Kaguya Ōtsutsuki. His leadership united the shinobi nations,
+          ending generations of conflict. Despite personal struggles, Naruto
+          turned his pain into strength, becoming a beacon of hope and proving
+          that hard work and perseverance could overcome any obstacle.`,
+  },
+  {
+    id: 1,
+    summaryDate: "October 7, 2026 • 8:37 PM",
+    wordsCount: 67,
+    charactersCount: 345,
+    summaryText: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum quis nam dicta nisi et adipisci aliquam deserunt, quibusdam nostrum quasi tempore enim. Vitae, odit deserunt voluptate quibusdam qui est necessitatibus ea rerum exercitationem laborum expedita deleniti itaque minus possimus dolorem, quidem ex reprehenderit. Accusamus beatae suscipit ipsum culpa, excepturi nisi inventore doloremque expedita nam perferendis, delectus illum consectetur! Laborum ex unde quibusdam! Quos similique perferendis temporibus, dolorum mollitia sapiente illum sed consectetur magni, tempora incidunt at debitis?`,
+  },
+  {
+    id: 2,
+    summaryDate: "January 18, 2026 • 11:54 AM",
+    wordsCount: 98,
+    charactersCount: 612,
+    summaryText: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id error fuga dicta explicabo tenetur amet laudantium, molestiae corrupti harum veniam nobis, eum, sapiente quisquam in nam beatae magnam doloremque cum? Nesciunt porro illo perferendis aperiam ab, sed hic. Quo incidunt, vel, modi exercitationem aliquid laboriosam doloribus quis odit aspernatur eos suscipit pariatur sit deleniti, est impedit minus illum repudiandae facilis voluptas sunt! Nostrum, sit nulla minima eos voluptates quaerat, laborum quod vero ex porro, adipisci quasi ipsam.`,
+  },
+]);
