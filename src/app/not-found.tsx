24403a35ca6621e9de383179635a7b5abc095c31
EndyPlus <aapliusnin@gmail.com>
2026-03-16 22:49:40 +0200

    update: 404 page changed and adapted.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,30 +5,22 @@ import { IconLogo } from "../components/ui/Icons";
 export default function NotFound() {
   return (
     <div className="bg-main-light flex h-full items-center justify-center">
-      <div className="border-accent-light flex flex-col rounded-2xl border bg-white px-15 py-10 text-center">
+      <div className="border-accent-light xs:w-auto xs:px-15 xs:py-10 flex w-[90vw] flex-col rounded-2xl border bg-white px-10 py-8 text-center">
         <IconLogo size={60} className="shadow-icon self-center" />
         <div className="my-8">
-          <h3 className="text-heading tracking-base text-black-base leading-9 font-medium">
+          <h3 className="xs:text-heading tracking-base text-black-base text-xl leading-9 font-medium">
             404 - Page Not Found
           </h3>
-          <p className="tracking-base text-black-accent mt-2 leading-4">
+          <p className="tracking-base text-black-accent leading-base mt-2">
             This page does not exist.
           </p>
         </div>
-        <div className="flex gap-x-8">
-          <Link
-            href="/home"
-            className="text-white-base bg-black-secondary shadow-input rounded-large flex w-full cursor-pointer items-center justify-center px-1 py-0.5"
-          >
-            Home
-          </Link>
-          <Link
-            href="/history"
-            className="text-white-base bg-black-secondary shadow-input rounded-large flex w-full cursor-pointer items-center justify-center px-1 py-0.5"
-          >
-            History
-          </Link>
-        </div>
+        <Link
+          href="/home"
+          className="text-white-base bg-black-secondary leading-base tracking-base shadow-input rounded-large xs:w-full tabulating-element flex w-2/3 cursor-pointer items-center justify-center self-center px-2 py-1 font-medium capitalize"
+        >
+          Return back
+        </Link>
       </div>
     </div>
   );
